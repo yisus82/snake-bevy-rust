9a3b0ed23f6a2f5083005683120efab749e74cb1
yisus82 <djalma8@gmail.com>
2022-10-03 09:18:09 +0200

    Move snake

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,26 @@ fn spawn_snake(mut commands: Commands) {
         .insert(SnakeHead);
 }
 
+fn snake_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut head_positions: Query<&mut Transform, With<SnakeHead>>,
+) {
+    for mut transform in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::Left) {
+            transform.translation.x -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Right) {
+            transform.translation.x += 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Down) {
+            transform.translation.y -= 2.;
+        }
+        if keyboard_input.pressed(KeyCode::Up) {
+            transform.translation.y += 2.;
+        }
+    }
+}
+
 fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(Camera2dBundle::default());
 }
@@ -42,5 +62,6 @@ fn main() {
         .add_startup_system(spawn_snake)
         .add_plugins(DefaultPlugins)
         .add_system(close_on_esc)
+        .add_system(snake_movement)
         .run();
 }
