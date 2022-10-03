51cc811266635e5e2daa1d2438a0c0a4828e05ea
yisus82 <djalma8@gmail.com>
2022-10-03 09:13:26 +0200

    Spawn snake

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 use bevy::{
     prelude::*,
-    window::{PresentMode, WindowMode},
+    window::{close_on_esc, PresentMode, WindowMode},
 };
+
+const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+
+#[derive(Component)]
+struct SnakeHead;
+
+fn spawn_snake(mut commands: Commands) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: SNAKE_HEAD_COLOR,
+                ..default()
+            },
+            transform: Transform {
+                scale: Vec3::new(10.0, 10.0, 10.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(SnakeHead);
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(Camera2dBundle::default());
+}
 
 fn main() {
     App::new()
@@ -14,10 +39,8 @@ fn main() {
             ..default()
         })
         .add_startup_system(setup_camera)
+        .add_startup_system(spawn_snake)
         .add_plugins(DefaultPlugins)
+        .add_system(close_on_esc)
         .run();
-}
-
-fn setup_camera(mut commands: Commands) {
-    commands.spawn_bundle(Camera2dBundle::default());
 }
